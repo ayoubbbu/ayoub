6f5ac85dc774b00ba3b80cfcc6c7e09f013eab5b
ayoubboukadida <70900383+ayoubbbu@users.noreply.github.com>
2021-06-22 10:45:29 +0100

    Update ayoub.py

--- ayoub.py
+++ ayoub.py
@@ -1,13 +1,10 @@
-print("ayoub boukadida is kinge")
-a=2
-print(a)
-b=5
-print(b)
-import ayoub
-print(type(a))
-a,b,c=1,2,3
-print(a,b,c)
-
-
-name = input("ayoub")
-
+print("                                                          _  _
+                                 ^           |         |       /      \
+          ^        |     |    /     \        |         |      |        |
+         / \        \   /    /       \       |         |      |       /
+     ---/---\---       |    |         |      |         |      |------
+       /     \         |    |         |      |         |      |       \
+      /       \        |     \       /        \        /      |        |
+                              \     /            _  _          \      /
+                                 •                                - -
+      ")
